4c407b3d507fb02863a52221d741fd207fdf672e
jastaffen <jack.staffen@gmail.com>
2019-10-22 15:26:27 -0400

    hid SQL logger display

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ Bundler.require
 
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
+old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
 
 require_all 'lib'
 
