4d320b22b8c3c7f130c94deebdf0c45b46090d37
Barbara <muesingb@gmail.com>
2019-10-23 09:42:25 -0400

    shortened code

--- bin/methods.rb
+++ bin/methods.rb
@@ -1,15 +1,11 @@
 require 'tty'
 
-
 def welcome
-    puts 
-    puts
+    2.times {puts}
     puts "Hello there!"
-    puts
-    puts
+    2.times {puts}
     Catpix::print_image "Flatiron-Logo.jpg"
 end
-
 
 def start
     prompt = TTY::Prompt.new
@@ -42,13 +38,11 @@ def storyline
     keyhit.keypress("press a key")
     puts
     options = TTY::Prompt.new
-    puts
-    puts
+    2.times {puts}
     decision_text = options.select("Choose:", Scene.first.options.map {|option| option.text})
     choice = Option.find_by(text: decision_text)
     uo1 = UserOption.create(user_id: @u1.id, option_id: choice.id)
-    puts
-    puts
+    2.times {puts}
     while choice.to_scene do
         puts "#{choice.to_scene.story}"
         puts
@@ -56,32 +50,26 @@ def storyline
         keyhit2.keypress("press something!")
         puts
         options = TTY::Prompt.new
-        puts
-        puts
+        2.times {puts}
         decision_text = options.select("Choose:", choice.to_scene.options.map {|option| option.text})
         choice = Option.find_by(text: decision_text)
         uo1 = UserOption.create(user_id: @u1.id, option_id: choice.id)
-        puts
-        puts
+        2.times {puts}
         if choice.to_scene_id == 11 || choice.to_scene_id == 12
             puts "#{choice.to_scene.story}"
             next_scene = TTY::Prompt.new
-            puts
-            puts
+            2.times {puts}
             next_scene.keypress("Press any key to continue")
             break
         end
     end
-    puts
-    puts
+    2.times {puts}
     puts "**"
     puts 
     puts "#{Scene.last.story}"
     puts 
     puts "**"
 end
-
-
 
 def runner
     welcome
@@ -90,7 +78,7 @@ def runner
         @u1 = user_login
         puts "=" * 50
         puts
-        puts "Howdy #{@u1.name}!"
+        puts "Howdy #{@u1.name}! 🤠"
         yes_or_no = TTY::Prompt.new
         yes_or_no.keypress("Ready to begin Flatiron Simulator 9,000? Press any key to begin!")
         puts
@@ -107,14 +95,13 @@ def runner
         puts "~" * 50
         puts 
         storyline
-        puts
-        puts
+        2.times {puts}
         view_your_stats = TTY::Prompt.new
         stats = view_your_stats.select("Do you want to view your experience?", ["yes", "no"])
-        puts
-        puts
+        2.times {puts}
         if stats == "yes"
             puts "HERE IS WHAT YOU DID, #{@u1.name}!"
+            2.times {puts}
             @u1.options.each { |option| puts "#{option.from_scene.story} \n \n #{option.text} \n \n" }
             puts 
             puts "#{@u1.options.last.to_scene.story}"
